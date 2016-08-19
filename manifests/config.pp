9920f508b8f5a52450a886de5c1b249733f3d66f
Tim Meusel <tim@bastelfreak.de>
2016-08-19 17:52:26 +0200

    fix(#4) dont hardcode file path
    this got initially fixed in
    https://github.com/voxpupuli/puppet-graphite_powershell/pull/5/commits/241c1b839bf81a3c8ad411e8d4c587279ed9272a
    by @drmarconi

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,7 +9,7 @@
 #
 class graphite_powershell::config {
 
-  file { 'C:/GraphitePowershell/StatsToGraphiteConfig.xml':
+  file { "${graphite_powershell::params::install_dir}/StatsToGraphiteConfig.xml":
     ensure  => present,
     owner   => 'SYSTEM',
     content => template('graphite_powershell/StatsToGraphiteConfig.xml.erb'),
